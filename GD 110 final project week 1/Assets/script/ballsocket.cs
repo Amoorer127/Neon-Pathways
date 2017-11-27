4cc45e0c9e93e515a5251c9022ca0700bb40b2db
jesusr96 <jesus9r6@gmail.com>
2017-11-27 16:24:07 -0500

    here the game

---
+++ GD 110 final project week 1/Assets/script/ballsocket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballsocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider bluezone){
+
+		if ( gameObject.tag == ("blue")){
+			Debug.Log ("ball is in the blue");
+		
+		}
+	}
+
+}
---
+++ GD 110 final project week 1/Assets/script/leveltime.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class leveltime : MonoBehaviour
+{
+    private int timeLeft = 180;
+    public Text countdownText;
+
+	float starttime;
+	float duration;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+		duration = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = ("Time Left = " + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times Up!";
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+
+	void OnTriggerEnter(Collider daedzone){
+		starttime = Time.time;
+
+	}
+	void OnTriggerStay(Collider daedzone){
+		if (Time.time >= starttime + duration) {
+
+			Debug.Log ("timeup");
+		}
+
+	}
+}
